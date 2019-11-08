60a56180ccacedf229cb6ddafc90af65967966b5
Zifei Jiang <zifei@wsz-l.nonlin.local>
2019-11-07 17:14:56 -0700

    first

---
+++ slamros/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/local/NONLIN/zifei/code_repo/slamros/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
